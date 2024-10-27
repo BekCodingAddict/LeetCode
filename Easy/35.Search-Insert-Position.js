c9b0af8bd93cb1ad169712127ee39ea925602002
unknown <experte001@gmail.com>
2024-10-28 03:00:10 +0900

    EASY: 66.Plus One

--- Easy/35.Search-Insert-Position.js
+++ Easy/35.Search-Insert-Position.js
@@ -56,3 +56,4 @@ console.log(searchInsert([1, 3, 5, 6], 5));
 console.log(searchInsert([1, 3, 5, 6], 2));
 console.log(searchInsert([1, 3, 5, 6], 7));
 console.log(searchInsert([1, 3], 2));
+console.log(9 / 4);
